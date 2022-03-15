6764a9bcc9e88656c6c74e20049ad81758345174
igor_kantorski <igor.kantorski@globedata.pl>
2022-03-15 15:15:02 +0100

    - Added exclude_private_from_slots config variable - Handled `exclude_private_from_slots` inside `inherit_config` to not be inherited from parents - Rendering `__slots__` inside `ModelMetaclass.__new__` method from private attributes is now conditional - depends on `exclude_private_from_slots` config variable - BaseModel.__repr_args__ is now aware that not all __dict__ members are actualy members of __fields__. Some private attributes may be stored in __dict__ instead of __slots__.

--- pydantic/main.py
+++ pydantic/main.py
@@ -280,7 +280,8 @@ class ModelMetaclass(ABCMeta):
             '__json_encoder__': staticmethod(json_encoder),
             '__custom_root_type__': _custom_root_type,
             '__private_attributes__': {**base_private_attributes, **private_attributes},
-            '__slots__': slots | private_attributes.keys(),
+            '__slots__': slots
+            | {key for key in private_attributes.keys() if key not in config.exclude_private_from_slots},
             '__hash__': hash_func,
             '__class_vars__': class_vars,
             **{n: v for n, v in namespace.items() if n not in exclude_from_namespace},
@@ -876,8 +877,11 @@ class BaseModel(Representation, metaclass=ModelMetaclass):
             return self.dict() == other
 
     def __repr_args__(self) -> 'ReprArgs':
+        # not all private attributes are stored in slots
         return [
-            (k, v) for k, v in self.__dict__.items() if k not in self.__fields__ or self.__fields__[k].field_info.repr
+            (k, v)
+            for k, v in self.__dict__.items()
+            if k not in self.__fields__ or (k in self.__fields__ and self.__fields__[k].field_info.repr)
         ]
 
 
